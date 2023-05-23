ae83c507dcfe86c649205310eac781c61fd03f80
vijay <vijay.ja@aarvm.com>
2023-05-23 17:13:02 +0530

    added second component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import "./styles.scss";
 import ContactHeader from "./Components/Contact/ContactHeader";
+import AddressLocation from "Components/Contact/AddressLocation";
 
 export default function App() {
     return (
-        <ContactHeader />
+        <>
+            <ContactHeader />
+            <AddressLocation />
+        </>
     );
 }
 
