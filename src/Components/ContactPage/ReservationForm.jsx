60253b4e3500a210ab1024acb19cd052ff2026a8
vijay <vijay.ja@aarvm.com>
2023-05-26 06:42:35 +0530

    added state to reservation-form component

--- src/Components/ContactPage/ReservationForm.jsx
+++ src/Components/ContactPage/ReservationForm.jsx
@@ -1,8 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../styles.scss";
 import mint from "../../assests/images/Mint.png"
 
 export default function ReservationForm() {
+
+    const [formData, setFormData] = useState({
+        firstName: "",
+        lastName: "",
+        mail: "",
+        phone: "",
+        date: "",
+        time: "",
+        noOfPersons: 0
+    })
+
+
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        alert(JSON.stringify(formData));
+    }
+
+
     return (
         <div className="form">
             <div className="form-header">
@@ -14,16 +40,16 @@ export default function ReservationForm() {
             </div>
             <div className="form-input">
                 <div className="name">
-                    <input className="first-name" placeholder="First Name"></input>
-                    <input className="last-name" placeholder="Last Name"></input>
+                    <input className="first-name" name="firstName" placeholder="First Name" onChange={handleChange} value={formData.firstName}></input>
+                    <input className="last-name" name="lastName" placeholder="Last Name" onChange={handleChange} value={formData.lastName}></input>
                 </div>
                 <div className="mail-and-phone">
-                    <input className="mail" placeholder="Email"></input>
-                    <input className="phone" placeholder="Phone"></input>
+                    <input className="mail" name="mail" placeholder="Email" onChange={handleChange} value={formData.mail}></input>
+                    <input className="phone" name="phone" placeholder="Phone" onChange={handleChange} value={formData.phone}></input>
                 </div>
                 <div className="schedule">
-                    <input type="date" className="select-date" placeholder="Choose the date"></input>
-                    <select className="time">
+                    <input type="date" className="select-date" name="date" placeholder="Choose the date" onChange={handleChange} value={formData.date}></input>
+                    <select className="time" name="time" onChange={handleChange} value={formData.time}>
                         <option>11:00 am</option>
                         <option>13:00 pm</option>
                         <option>14:00 pm</option>
@@ -33,7 +59,7 @@ export default function ReservationForm() {
                         <option>19:00 pm</option>
                     </select>
                 </div>
-                <select className="no-of-persons">
+                <select className="no-of-persons" name="noOfPersons" onChange={handleChange} value={formData.noOfPersons}>
                     <option>2</option>
                     <option>4</option>
                     <option>6</option>
@@ -41,7 +67,7 @@ export default function ReservationForm() {
                     <option>10</option>
                     <option>15</option>
                 </select>
-                <button className="book-now">Book Now</button>
+                <button className="book-now" onClick={handleSubmit}>Book Now</button>
             </div>
         </div>
     )
