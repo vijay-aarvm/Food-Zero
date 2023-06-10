db863a7f718d20d0e967978f6bc4195d091acbf1
vijay <vijay.ja@aarvm.com>
2023-06-10 23:21:26 +0530

    added portfolio content page

---
+++ src/Components/SinglePortfolio/NavigatePortfolio.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "../../styles.scss";
+import iconback from "../../assests/images/Icon_back.png";
+
+export default function NavigatePortfolio() {
+    return (
+        <div className="navigate-portfolio">
+            <div className="previous">
+                <p className="previous-page">← PREVIOUS PAGE</p>
+                <p className="previous-portfolio">Strip Steak With Rosemary Butter</p>
+            </div>
+            <button className="icon-back-btn"><img className="icon-back" src={iconback} alt="icon-back" /></button>
+            <div className="next">
+                <p className="next-page">NEXT PAGE →</p>
+                <p className="next-portfolio">Option of natural wine available</p>
+            </div>
+        </div>
+    )
+}
--- src/Components/SinglePortfolio/SinglePortfolio.tsx
+++ src/Components/SinglePortfolio/SinglePortfolio.tsx
@@ -2,12 +2,16 @@ import React from "react";
 import "../../styles.scss";
 import SinglePortfolioHeader from "./SinglePortfolioHeader";
 import SinglePortfolioContent from "./SinglePortfolioContent";
+import NavigatePortfolio from "./NavigatePortfolio";
+import Footer from "../CommonComponents/Footer";
 
 export default function SinglePortfolio() {
     return (
         <div>
             <SinglePortfolioHeader />
             <SinglePortfolioContent />
+            <NavigatePortfolio />
+            <Footer />
         </div>
     )
 }
