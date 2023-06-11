3a42deae8d5edc013790463da4af5deb8d8abc78
vijay <vijay.ja@aarvm.com>
2023-06-11 13:32:45 +0530

    added responsiveness to about page

--- src/Components/About/AboutProcess.tsx
+++ src/Components/About/AboutProcess.tsx
@@ -16,14 +16,14 @@ export default function AboutProcess() {
                 <img className="meat-sliced" src={meatsliced} alt="meat sliced" />
                 <div className="about-slice">
                     <p className="slice-title">01.Slice</p>
-                    <p className="about-slice">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Purus lorem id penatibus imperdiet. Turpis egestas ultricies purus auctor tincidunt lacus nunc. Convallis pellentesque quis fringilla sagittis. Egestas in risus sit nunc nunc, arcu donec nam etiam.</p>
+                    <p className="aboutslice">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Purus lorem id penatibus imperdiet. Turpis egestas ultricies purus auctor tincidunt lacus nunc. Convallis pellentesque quis fringilla sagittis. Egestas in risus sit nunc nunc, arcu donec nam etiam.</p>
                 </div>
             </div>
             <div className="pickle-and-bake">
                 <div className="bake">
                     <div className="about-bake">
                         <p className="bake-title">03.Bake</p>
-                        <p className="about-bake">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Purus lorem id penatibus imperdiet.</p>
+                        <p className="aboutbake">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Purus lorem id penatibus imperdiet.</p>
                     </div>
                     <img className="meat-baked" src={meatbaked} alt="meat baked" />
                 </div>
@@ -31,7 +31,7 @@ export default function AboutProcess() {
                     <img className="meat-pickled" src={meatpickled} alt="meat pickled" />
                     <div className="about-pickle">
                         <p className="pickle-title">02.pickled</p>
-                        <p className="about-pickle">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Purus lorem id penatibus imperdiet.</p>
+                        <p className="aboutpickle">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Purus lorem id penatibus imperdiet.</p>
                     </div>
                 </div>
             </div>
