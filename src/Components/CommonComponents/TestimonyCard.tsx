a8575a0f2509c0cc7521f819cd585da5fe1bfd2b
vijay <vijay.ja@aarvm.com>
2023-06-14 09:02:54 +0530

    added css to select nth child in comment section

---
+++ src/Components/CommonComponents/TestimonyCard.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+export default function TestimonyCard() {
+    return (
+        <div>
+            <div className="testimony-content">
+                <h3 className="client-testimony">“ Lorem ipsum dolor sit amet, consectetur adipiscing elit. Purus lorem id
+                    penatibus imperdiet. Turpis
+                    egestas ultricies purus Lorem ipsum dolor sit amet .
+                </h3>
+            </div>
+            <div className="client">
+
+            </div>
+        </div>
+    )
+}
