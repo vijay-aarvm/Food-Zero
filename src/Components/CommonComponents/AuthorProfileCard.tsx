308b9a8052edc04d07038d74ca9b55d850c7d9d1
vijay <vijay.ja@aarvm.com>
2023-06-16 18:45:36 +0530

    changed the obj type in AuthorProfileCard

--- src/Components/CommonComponents/AuthorProfileCard.tsx
+++ src/Components/CommonComponents/AuthorProfileCard.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import "./authorProfileCard.scss";
 
+
 export default function AuthorProfileCard({ obj }: any) {
+
+    const { authorProfile, authorName, blogPostedDate, blogPostedTime, totalComments } = obj;
+
     return (
         <div className="blog-author">
-            <img className="author-pic" src={obj.authorProfile} alt="author" />
-            <p className="author-name">{obj.authorName}</p>
+            <img className="author-pic" src={authorProfile} alt="author" />
+            <p className="author-name">{authorName}</p>
             <ul className="author-details">
-                <li className="posted-on">{obj.blogPostedDate}</li>
-                <li className="posted-time">{obj.blogPostedTime}</li>
-                <li className="no-of-comments">{obj.totalComments}</li>
+                <li className="posted-on">{blogPostedDate}</li>
+                <li className="posted-time">{blogPostedTime}</li>
+                <li className="no-of-comments">{totalComments}</li>
             </ul>
         </div>
     )
