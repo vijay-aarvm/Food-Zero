12b7498dd50c356c2cb4e9eb9247c3dea888545e
vijay <vijay.ja@aarvm.com>
2023-06-17 21:27:30 +0530

    modified TypeAndCount

--- src/Components/SinglePost-Sidebar/TypeAndCount.tsx
+++ src/Components/SinglePost-Sidebar/TypeAndCount.tsx
@@ -2,19 +2,6 @@ import React from "react";
 import "./typeAndCount.scss";
 
 export default function TypeAndCount({ title, obj }: any) {
-    const endsWithY = title.endsWith("y");
-
-    const updatedTitle = (endsWithY ? `${title.slice(0, -1)}ies` : `${title}s`).toLowerCase();
-
-    // let name, count;
-
-    // if (type === "category") {
-    //     name = obj.name;
-    //     count = obj.count;
-    // } else if (type === "archive") {
-    //     name = obj.month;
-    //     count = obj.archiveCount;
-    // }
 
     return (
         <div className="type-and-count-card">
