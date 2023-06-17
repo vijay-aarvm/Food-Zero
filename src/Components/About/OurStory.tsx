3a4a67b719570e612c8af794e3cdbd2413e26524
vijay <vijay.ja@aarvm.com>
2023-06-17 17:52:51 +0530

    changed class names in our team section, added primary and secondary fonts and used it throughout the app

--- src/Components/About/OurStory.tsx
+++ src/Components/About/OurStory.tsx
@@ -16,17 +16,17 @@ export default function AboutUs() {
                 <img className="ourstory-img" src={ourstory} alt="ourstory" />
             </div>
             <div className="our-team">
-                <div className="our-manager">
-                    <p className="manager-title">Restuarant Manager</p>
-                    <p className="manager-name">Carson Hugn</p>
-                    <img className="manager" src={manager} alt="manager" />
+                <div className="our-manager our-employee">
+                    <p className="manager-title employee-title">Restuarant Manager</p>
+                    <p className="manager-name employee-name">Carson Hugn</p>
+                    <img className="manager employee-photo" src={manager} alt="manager" />
                     <p className="about-manager">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et in sed in pellentesque ornare nunc nisl. Augue habitant accumsan, ut parturient orci ac etiam congue mi. </p>
                 </div>
-                <div className="our-chef">
+                <div className="our-chef our-employee">
                     <p className="about-chef">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et in sed in pellentesque ornare nunc nisl. Augue habitant accumsan, ut parturient orci ac etiam congue mi. </p>
-                    <p className="chef-title">Executive Chef</p>
-                    <p className="chef-name">Jane Cooper</p>
-                    <img className="chef" src={chef} alt="chef" />
+                    <p className="chef-title employee-title" >Executive Chef</p>
+                    <p className="chef-name employee-name">Jane Cooper</p>
+                    <img className="chef chef-photo" src={chef} alt="chef" />
                 </div>
             </div>
             <div className="our-food-video">
